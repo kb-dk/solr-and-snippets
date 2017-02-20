7497280f988559ed930af19cf57491df923336f5
Sigfrid Lundberg <siggelundberg@gmail.com>
2017-02-20 13:40:57 +0100

    fixed details

--- indexing/solr_updater.pl
+++ indexing/solr_updater.pl
@@ -12,17 +12,15 @@ my $file_list         = "";
 my $delete_all        = "";
 my %param;
 
-my $solrizer_template = "http://localhost:8080/exist/rest/db/{service}/present.xq{?op,file,c}";
-my $solr_template     = "http://localhost:8983/solr/{collection}/update{?softCommit}";
+my $solrizer_template = URI::Template->new("http://{exist_host_port}/exist/rest/db/{service}/present.xq{?op,file,c}");
+my $solr_template     = URI::Template->new("http://{solr_host_port}/solr/{collection}/update{?softCommit}");
 
 my $result = GetOptions (
-    "exist-host-port=s"  =>  \$exist_host_port,
-    "solr-host-port=s"   =>  \$solr_host_port,
     "file-list=s"        =>  \$file_list,
     "delete-all=s"       =>  \$delete_all,
-    "exist-collection=s" =>  \$exist_collection,
     "param=s"            =>  \%param);
 
+$param{'softCommit'}='true';
 
 $ua->agent("adl_solr_client/0.1 ");
 
@@ -38,7 +36,7 @@ if($file_list) {
 #
 ################################################################################################
 
-my $solr_xml="$solr_collection/update?$solr_commit";
+my $solr_xml=  $solr_template->process(%param);
 
 if($delete_all && &promptUser("Really delete index (y/n)","n")) {
 
@@ -77,13 +75,11 @@ while(my $file=<$list>) {
 
 
 sub url() {
-    my $coll = shift;
-    my $file = shift;
+    my $template = shift;
 
-    my $exist_solrizer = "http://bifrost-test-01.kb.dk:8080/exist/rest/db/adl/present.xq?c=". $coll .
-                          "&op=solrize&doc=" . $file;
+    my $uri =  $template->process( %param );  
 
-    return  $exist_solrizer;
+    return  $uri;
 			  
 }
 
@@ -98,7 +94,7 @@ sub get_it() {
     my $f = '';
 
     ($c,$f) = split(/\//,$file);
-    my $exist_xml = &url($c,$f);
+    my $exist_xml = &url($solrizer_template);
     my $get_req = HTTP::Request->new(GET => $exist_xml);
     my $response = $ua->request($get_req);
     print $response->status_line . "\n";
