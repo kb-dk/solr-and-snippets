6863fd082c5283c6d29532bc1268c6eaea53d99a
Sigfrid Lundberg <siggelundberg@gmail.com>
2017-02-21 10:47:18 +0100

    solr_update.pl works nicely with URI::Templates

--- indexing/solr_updater.pl
+++ indexing/solr_updater.pl
@@ -12,8 +12,8 @@ my $file_list         = "";
 my $delete_all        = "";
 my %param;
 
-my $solrizer_template = URI::Template->new("http://{exist_host_port}/exist/rest/db/{service}/present.xq{?op,file,c}");
-my $solr_template     = URI::Template->new("http://{solr_host_port}/solr/{collection}/update{?softCommit}");
+my $solrizer_template = URI::Template->new("http://{exist_host}:{exist_port}/exist/rest/db/{service}/present.xq{?op,doc,c}");
+my $solr_template     = URI::Template->new("http://{solr_host}:{solr_port}/solr/{collection}/update{?softCommit}");
 
 my $result = GetOptions (
     "file-list=s"        =>  \$file_list,
@@ -36,7 +36,7 @@ if($file_list) {
 #
 ################################################################################################
 
-my $solr_xml=  $solr_template->process(%param);
+my $solr_uri =  $solr_template->process(%param);
 
 if($delete_all && &promptUser("Really delete index (y/n)","n")) {
 
@@ -44,7 +44,7 @@ if($delete_all && &promptUser("Really delete index (y/n)","n")) {
     
 # Create a delete request
 
-    my $del_req = HTTP::Request->new(POST => $solr_xml);
+    my $del_req = HTTP::Request->new(POST => $solr_uri);
     $del_req->content_type('text/xml; charset=utf-8');
     $del_req->content($solr_del);
 
@@ -74,28 +74,19 @@ while(my $file=<$list>) {
 
 
 
-sub url() {
-    my $template = shift;
-
-    my $uri =  $template->process( %param );  
-
-    return  $uri;
-			  
-}
-
 sub get_it() {
     my $file = shift;
-
-#	$del_req->content_type('text/xml; charset=utf-8');
-#	$del_req->content($solr_del);
 
     $file =~ s|^.*?adl/||;
     my $c = '';
     my $f = '';
 
     ($c,$f) = split(/\//,$file);
-    my $exist_xml = &url($solrizer_template);
-    my $get_req = HTTP::Request->new(GET => $exist_xml);
+    $param{'doc'}=$f;
+    $param{'c'}=$c;
+    my $exist_uri = $solrizer_template->process(%param);
+    print STDERR "$exist_uri\n";
+    my $get_req = HTTP::Request->new(GET => $exist_uri);
     my $response = $ua->request($get_req);
     print $response->status_line . "\n";
 
@@ -110,8 +101,8 @@ sub send_it() {
     print "$file\n";
 
 # Create an update request
-    print $solr_xml . "\n";
-    my $req = HTTP::Request->new(POST => $solr_xml);
+    print STDERR $solr_uri . "\n";
+    my $req = HTTP::Request->new(POST => $solr_uri);
     $req->content_type('text/xml; charset=utf-8');
     $req->content($content);
 
--- run_indexing.sh
+++
@@ -1,9 +0,0 @@
-
-
-indexing/solr_updater.pl \
-    --file-list=files.text \
-    --param exist_host_port=localhost:8080 \
-    --param service=adl \
-    --param op=solrize \
-    --param solr_host_port=localhost:
-collection
