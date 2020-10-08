fa58333cfa8eff11f1935680aa66a40c35637e0d
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-08 13:04:02 +0200

    those zoo*.cfg are derived data, sorry!

--- solr/zookeepers/generate-configs.sh
+++ solr/zookeepers/generate-configs.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+#
+# Makes three config files for each of three zookeepers
+#
 
 for instance in {1..3}
 do
-   m4 -DINSTANCE=$instance -DNUMBER=3 -DHOME=/home/zookeeper -DHOST=index-test-0  -DDOMAIN=.kb.dk zoo-make-config.m4 
+    m4 -DINSTANCE=$instance \
+       -DNUMBER=3 \
+       -DHOME=/home/zookeeper \
+       -DHOST=index-test-0  \
+       -DDOMAIN=.kb.dk zoo-make-config.m4 > "zoo$instance.cfg"
 done
 
 		
