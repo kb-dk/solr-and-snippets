8cb5c284581328f9191a853b0a8300cf6368313b
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-09 09:11:45 +0200

    added the service files as well

--- solr/zookeepers/generate-configs.sh
+++ solr/zookeepers/generate-configs.sh
@@ -4,16 +4,20 @@
 # Makes three config files for each of three zookeepers
 #
 
+ZHOME=/home/zookeeper
+ZHOST=index-test-0
+ZJAVA=/etc/alternatives/jre_1.8.0  
+
 for instance in {1..3}
 do
     m4 -DINSTANCE=$instance \
        -DNUMBER=3 \
-       -DHOME=/home/zookeeper \
-       -DHOST=index-test-0  \
+       -DHOME=$ZHOME \
+       -DHOST=$ZHOST  \
        -DDOMAIN=.kb.dk zoo-make-config.m4 > "zoo$instance.cfg"
 
     m4 -DINSTANCE=$instance \
-       -DJAVA_DIR=/etc/alternatives/jre_1.8.0  zookeeper_systemd_service.m4 > "zookeeper$instance.service"
+       -DJAVA_DIR=$ZJAVA zookeeper_systemd_service.m4 > "zookeeper$instance.service"
 done
 
 		
