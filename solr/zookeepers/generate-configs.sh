47435c63f7ba5439d9b2a3bdf01e79a50eb05071
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-12 15:50:13 +0200

    more howto comments

--- solr/zookeepers/generate-configs.sh
+++ solr/zookeepers/generate-configs.sh
@@ -4,8 +4,16 @@
 # Makes three config files for each of three zookeepers
 #
 
+# The zookeeper homes will become /home/zookeeper<i>, i=1,2,3
 ZHOME=/home/zookeeper
+
+# Note index-test-0 will be extended with index i=1,2,3
 ZHOST=index-test-0
+
+# The hostname will be suffixed with the ZDOMAIN
+ZDOMAIN=.kb.dk
+
+# The the systemctl and others where you java is
 # ZJAVA=/usr/local/java/latest
 ZJAVA=/etc/alternatives/jre_1.8.0 
 
@@ -15,7 +23,7 @@ do
        -DNUMBER=3 \
        -DHOME=$ZHOME \
        -DHOST=$ZHOST  \
-       -DDOMAIN=.kb.dk zoo-make-config.m4 > "zoo$instance.cfg"
+       -DDOMAIN=$ZDOMAIN zoo-make-config.m4 > "zoo$instance.cfg"
 
     m4 -DINSTANCE=$instance \
        -DJAVA_DIR=$ZJAVA zookeeper-systemd-service.m4 > "zookeeper$instance.service"
