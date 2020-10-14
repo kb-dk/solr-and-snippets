6fdd639026507cecb063c962212dee0b83318407
Sigfrid Lundberg <siggelundberg@gmail.com>
2020-10-14 11:15:43 +0200

    making it a little bit better

--- solr/zookeepers/generate-configs.sh
+++ solr/zookeepers/generate-configs.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #
-# Makes three config files for each of three zookeepers
+# Makes three config files for each of three zookeepers,
+# and three systemd startup scripts
 #
 
 # The zookeeper homes will become /home/zookeeper<i>, i=1,2,3
@@ -17,16 +18,39 @@ ZDOMAIN=.kb.dk
 # ZJAVA=/usr/local/java/latest
 ZJAVA=/etc/alternatives/jre_1.8.0 
 
+OUT=./test
+
+
+ARGS="--host hostname --domain domain name --java JAVA_HOME --home directory --out write results"
+
+if [ $? != 0 ] ; then echo "Please give $ARGS ..." >&2 ; exit 1 ; fi
+
+while true; do
+    echo $1
+    case "$1" in
+	-h | --host ) ZHOST="$2"; shift 2 ;;
+	-d | --domain ) ZDOMAIN="$2"; shift 2 ;;
+	-j | --java ) ZJAVA="$2"; shift 2 ;;
+	-t | --home ) ZHOME="$2"; shift 2 ;;
+	-o | --out  ) OUT="$2"; shift 2 ;;
+	* ) break ;;
+    esac
+done
+
+echo "host = $ZHOST ; domain = $ZDOMAIN ; java livs in = $ZJAVA ; zookeeper = $ZHOME ; writing in = $OUT"
+
+#exit 1
+
 for instance in {1..3}
 do
     m4 -DINSTANCE=$instance \
        -DNUMBER=3 \
        -DHOME=$ZHOME \
        -DHOST=$ZHOST  \
-       -DDOMAIN=$ZDOMAIN zoo-make-config.m4 > "zoo$instance.cfg"
+       -DDOMAIN=$ZDOMAIN zoo-make-config.m4 > "$OUT/zoo$instance.cfg"
 
     m4 -DINSTANCE=$instance \
-       -DJAVA_DIR=$ZJAVA zookeeper-systemd-service.m4 > "zookeeper$instance.service"
+       -DJAVA_DIR=$ZJAVA zookeeper-systemd-service.m4 > "$OUT/zookeeper$instance.service"
 done
 
 		
