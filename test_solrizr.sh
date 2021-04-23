1e46195c531fb572e8ee27afc9a218cdebd55305
Sigfrid Lundberg <siggelundberg@gmail.com>
2021-04-23 08:12:36 +0200

    testing letters-ng

--- test_solrizr.sh
+++ test_solrizr.sh
@@ -13,7 +13,7 @@ specimen[tfs]="../trykkefrihedsskrifter/tei_dir/1_013.xml"
 specimen[gv]="build/text-retriever/gv/1815_255/txt.xml"
 specimen[letters]="../letter-corpus/letter_books/001541111/001541111_000.xml"
 
-ed=gv
+ed=letters
 
 $SAXON -xsl:"build/text-retriever/$ed/solrize.xsl" -s:"${specimen[$ed]}" | xmllint --format  - 
 exit()
