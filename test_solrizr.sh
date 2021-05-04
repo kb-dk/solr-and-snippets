f04a92154e9054a3a917b12a40bd1143f295d9b4
Sigfrid Lundberg <siggelundberg@gmail.com>
2021-05-04 15:40:30 +0200

    handling error from snippet server

--- test_solrizr.sh
+++ test_solrizr.sh
@@ -9,8 +9,8 @@ declare -A specimen
 specimen[adl]="../public-adl-text-sources/texts/richardt03.xml"
 specimen[sks]="../SKS_tei/data/v1.9/ee1/txt.xml"
 specimen[tfs]="../trykkefrihedsskrifter/tei_dir/1_013.xml"
-# specimen[gv]="build/text-retriever/gv/1817_306_12/txt.xml"
-specimen[gv]="build/text-retriever/gv/1815_255/txt.xml"
+specimen[gv]="build/text-retriever/gv/1817_306_12/txt.xml"
+#specimen[gv]="build/text-retriever/gv/1815_255/txt.xml"
 #specimen[gv]="build/text-retriever/gv/1837_575/intro.xml"
 specimen[letters]="../letter-corpus/letter_books/001541111/001541111_000.xml"
 
