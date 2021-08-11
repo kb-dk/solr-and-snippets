8a111519ae9b31c9aab2684c06ccf6459b1b4f19
Sigfrid Lundberg <siggelundberg@gmail.com>
2021-08-11 15:33:40 +0200

    added ID on all kolofon.xml docs

--- utilities/traverse-and-transform.pl
+++ utilities/traverse-and-transform.pl
@@ -2,17 +2,18 @@
 
 use strict;
 
+my $saxon_xslt = "java -jar  /home/slu/saxon/saxon9he.jar ";
 my $sheet = "/home/slu/projects/solr-and-snippets/utilities/add-id.xsl";
 
 if(open(FIND,"find . -name '*.xml' -print |")) {
     while(my $file = <FIND>) {
 	next if $file =~ /schemas.xml/;
 	next if $file =~ /capabiliti.*xml/;
-	print $file;
+	print "\n# $file\n";
 	chomp $file;
 	my $out = $file."_shit";
-	system "saxon-xslt $file $sheet > $out";
-	system "xmllint --format $out > $file";
-	system "rm $out";
+	print "$saxon_xslt $file $sheet > $out" . "\n";
+	print  "xmllint --format $out > $file" . "\n";
+	print  "rm $out" . "\n";
     }
 }
